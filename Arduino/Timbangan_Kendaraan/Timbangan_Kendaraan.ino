4faf34d0bca79ecc2b2acb2b99ad0b771cbac021
Delynessence <riccopranata@gmail.com>
2024-10-10 19:32:29 +0700

    update bang

--- Arduino/Timbangan_Kendaraan/Timbangan_Kendaraan.ino
+++ Arduino/Timbangan_Kendaraan/Timbangan_Kendaraan.ino
@@ -4,7 +4,7 @@
 #include <HX711.h>
 #include <ESP8266WebServer.h>
 #include <WiFiClientSecureBearSSL.h>
-#include <ArduinoJson.h>
+#include <ArduinoJson.h> 
 
 #define DOUT D5
 #define CLK D6
@@ -12,16 +12,43 @@
 
 HX711 scale;
 LiquidCrystal_I2C lcd(0x27, 16, 2);
-ESP8266WebServer server(80);  // Ubah ke port 80 untuk HTTP
+ESP8266WebServer server(80); // Use HTTP port 80 for web server
 
 const char* ssid = "Magister";
 const char* password = "R!$np4n1viz";
+const char* googleScriptUrlWeight = "https://script.google.com/macros/s/AKfycbwQxCU4k1fJWlJ2RCM1B-jAo3nb1v3x9T0j4TXIYFweje8rYirzPKMba-v69v0hXh3f/exec"; // Real-time weight
 
-String vehicleType = "Unknown";
+String vehicleType = "";
 float currentWeight = 0;
-float maxWeight = 0;  
-float vehicleWeight = 0;  
-float calibrationFactor = 767964.63;
+float maxWeight = 0;
+float vehicleWeight = 0; 
+float calibrationFactor = 767964.63; // Hasil kalibrasi
+
+// Variabel berat kendaraan dan max muatan untuk jenis kendaraan
+struct Vehicle {
+  String name;
+  float maxWeight;
+  float vehicleWeight;
+};
+
+Vehicle vehicles[] = {
+  {"Truk", 10000, 5000},
+  {"Mobil", 2000, 1200},
+  {"Motor", 500, 150}
+};
+
+void sendDataToGoogleSheet(float netWeight) {
+  WiFiClientSecure client;
+  client.setInsecure();
+
+  String url = String(googleScriptUrlWeight) + "?weight=" + String(netWeight);
+  if (client.connect("script.google.com", 443)) {
+    client.print(String("GET ") + url + " HTTP/1.1\r\n" +
+                 "Host: script.google.com\r\n" +
+                 "Connection: close\r\n\r\n");
+    delay(500);
+  }
+}
 
 void setup() {
   Serial.begin(115200);
@@ -45,35 +72,38 @@ void setup() {
   
   scale.begin(DOUT, CLK);
   scale.set_scale(calibrationFactor);
-  scale.tare();
+  scale.tare(); 
   
   pinMode(BUZZER_PIN, OUTPUT);
-
+  
+  // Handler untuk menerima jenis kendaraan dari website
   server.on("/setVehicleType", HTTP_GET, []() {
-    if (server.hasArg("type")) {
-      vehicleType = server.arg("type");
-      if (vehicleType == "Car") {
-        maxWeight = 1000;
-      } else if (vehicleType == "Truck") {
-        maxWeight = 5000;
-      } else if (vehicleType == "Bike") {
-        maxWeight = 150;
-      } else {
-        vehicleType = "Unknown";
-        maxWeight = 0;
+    vehicleType = server.arg("vehicleType");
+    bool vehicleFound = false;
+
+    // Mencari jenis kendaraan yang cocok dan menetapkan berat serta maxWeight
+    for (Vehicle v : vehicles) {
+      if (v.name == vehicleType) {
+        maxWeight = v.maxWeight;
+        vehicleWeight = v.vehicleWeight;
+        vehicleFound = true;
+        break;
       }
-      
+    }
+
+    if (vehicleFound) {
       lcd.clear();
       lcd.setCursor(0, 0);
-      lcd.print("Veh: " + vehicleType);
+      lcd.print("Type: " + vehicleType);
       lcd.setCursor(0, 1);
-      lcd.print("Max: " + String(maxWeight) + " Kg");
-      server.send(200, "text/plain", "Vehicle: " + vehicleType + ", Max Weight: " + String(maxWeight));
+      lcd.print("MaxW: " + String(maxWeight) + " Kg");
+      server.send(200, "text/plain", "Vehicle type set to: " + vehicleType);
     } else {
-      server.send(400, "text/plain", "Vehicle type not provided");
+      server.send(400, "text/plain", "Invalid vehicle type.");
     }
   });
 
+  // Endpoint untuk menampilkan berat real-time di website
   server.on("/getWeight", HTTP_GET, []() {
     String response = String(currentWeight);
     server.send(200, "text/plain", response);
@@ -89,15 +119,16 @@ void loop() {
     currentWeight = scale.get_units(10);
     if (currentWeight < 0) currentWeight = 0;
 
-    float netWeight = currentWeight - vehicleWeight;
+    float netWeight = currentWeight - vehicleWeight; // Berat bersih
     lcd.clear();
     lcd.setCursor(0, 0);
     lcd.print("Weight: ");
     lcd.print(netWeight);
     lcd.print(" Kg");
 
-    // Kirim data ke website untuk real-time monitoring
-    // Tidak perlu koneksi baru ke Google Script
+    sendDataToGoogleSheet(netWeight); // Kirim data berat ke Google Sheet
+
+    // Cek jika berat bersih melebihi maxWeight
     if (netWeight > maxWeight) {
       lcd.setCursor(0, 1);
       lcd.print("Overload!");
@@ -108,6 +139,6 @@ void loop() {
       digitalWrite(BUZZER_PIN, LOW);
     }
 
-    delay(200);
+    delay(500);
   }
 }
